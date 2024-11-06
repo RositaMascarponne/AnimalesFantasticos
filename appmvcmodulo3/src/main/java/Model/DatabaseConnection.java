4b102bf990b52f0f39d8c507747db1db51bd0410
RositaMascarponne <rtirado.crn@artsgrafiques.org>
2024-11-06 15:07:09 +0100

    Iniciamos aqui el proyeco, con la databaseConnection ya configurada

---
+++ appmvcmodulo3/src/main/java/Model/DatabaseConnection.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Rosita
+ */
+public class DatabaseConnection {
+           // Método para obtener la conexión a la base de datos
+    public static Connection getConnection() throws SQLException {
+        Connection connection = null;
+        try {
+            // Cargar el driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            // Establecer la conexión
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3308/appmvcmodulo3?useSSL=false", // Desactivar SSL
+                    "root", 
+                    "root");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    } 
+}
---
+++ appmvcmodulo3/src/main/webapp/mod.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : mod
+    Created on : 6 nov 2024, 12:13:57
+    Author     : Rosita
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
